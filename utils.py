118c2efd1b548dc2f3d984cb0390ca370e874c8e
rronan <riochet@hotmail.com>
2015-12-30 17:14:09 +0100

    make it easier to read reports

--- utils.py
+++ utils.py
@@ -7,9 +7,12 @@ Created on Sun Dec  6 23:17:06 2015
 
 import numpy
 
-best_params = numpy.load('reports/best_params.npy')
-hyper_scores = numpy.load('reports/hyper_scores.npy')
-#print best_params
+report_folder = "reports_27_12/"
 
-report = numpy.load('reports/report_6.npy')
+best_params = numpy.load(report_folder + 'best_params.npy')
+hyper_scores = numpy.load(report_folder+ 'hyper_scores.npy')
+print best_params
+print hyper_scores
+
+report = numpy.load(report_folder + 'report_1.npy')
 print report
