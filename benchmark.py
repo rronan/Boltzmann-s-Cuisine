98d00f98100a3429fa9bafad0b7ca382b987f747
rronan <riochet@hotmail.com>
2016-01-05 12:19:57 +0100

    benchmark for server

--- benchmark.py
+++ benchmark.py
@@ -9,6 +9,8 @@ import timeit
 import numpy as np
 import random
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import BernoulliRBM
 
 data = np.load('train_data.npy')
 
@@ -35,6 +37,27 @@ start_time = timeit.default_timer()
 
 rf = RandomForestClassifier(n_jobs=-1, max_depth=100, n_estimators=200)
 rf.fit(np_train_set[:,20:], np.argmax(np_train_set[:,:20], axis=1))
-score = rf.score(np_test_set[:,20:], np.argmax(np_test_set[:,:20], axis=1))
+scorerf = rf.score(np_test_set[:,20:], np.argmax(np_test_set[:,:20], axis=1))
 
-print score,"in",timeit.default_timer() - start_time, "seconds."
+print "RandomForest: ", scorerf,"in",timeit.default_timer() - start_time, "seconds."
+
+#%%
+start_time = timeit.default_timer()
+
+
+lr = LogisticRegression(0.1, solver = 'lbfgs', multi_class='multinomial')
+lr.fit(np_train_set[:,20:], np.argmax(np_train_set[:,:20], axis=1))
+scorelr = lr.score(np_test_set[:,20:], np.argmax(np_test_set[:,:20], axis=1))
+
+print "LogisticRegression: ", scorelr,"in",timeit.default_timer() - start_time, "seconds."
+
+#%%
+start_time = timeit.default_timer()
+
+skrbm = BernoulliRBM(batch_size=20, learning_rate=0.01, 
+                     n_components=2000, n_iter=50,
+                     random_state=None, verbose=1)
+skrbm.fit(np_train_set[:,20:], np.argmax(np_train_set[:,:20], axis=1))
+scoreskrbm = skrbm.score(np_test_set[:,20:], np.argmax(np_test_set[:,:20], axis=1))
+
+print "BernoulliRBM: ", scoreskrbm,"in",timeit.default_timer() - start_time, "seconds."
