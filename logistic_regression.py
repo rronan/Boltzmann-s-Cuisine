f413fcec5b33a7bf400a9c06f399e3c5573930c6
rronan <riochet@hotmail.com>
2016-01-12 01:23:01 +0100

    pred and submission

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,28 +8,45 @@ Created on Sun Jan  3 22:56:53 2016
 from sklearn.linear_model import LogisticRegression as lr
 import numpy as np
 from preprocessing import create_submission
+from RBM import RBM
+from preprocessing import read_data
+from sklearn.preprocessing import LabelEncoder
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
     
+load_saved = True
 
-# This script needs two object in the environment: rbm, le
+train_data = np.load('train_data.npy')
 
-data = np.load('train_data_reduced.npy')
-Y = np.argmax(data[:,:20], axis=1)
-raw_X = data[:,20:]
-X = sigmoid(np.dot(raw_X, rbm.W) + rbm.hbias)
+if load_saved:
+    report = np.load("report.npy").item()    
+    rbm = RBM(len(train_data), report["n_hidden"], report["batch_size"])
+    rbm.W = report["W"]
+    rbm.hbias = report["hbias"]
+    rbm.vbias = report["vbias"]
 
-classifier = lr(0.1, solver = 'lbfgs', multi_class='multinomial')
+Y = np.argmax(train_data[:,:20], axis=1)
+train_data = train_data[:,20:]
+X = sigmoid(np.dot(train_data, rbm.W) + rbm.hbias)
+#X = train_data
+
+
+classifier = lr(0.01, solver = 'lbfgs', multi_class='multinomial')
 classifier.fit(X, Y)
 
-raw_Xtest = np.load('test_data_reduced.npy')
-Xtest = sigmoid(np.dot(raw_Xtest, rbm.W) + rbm.hbias)
+test_data = np.load('test_data.npy')
+test_X = sigmoid(np.dot(test_data, rbm.W) + rbm.hbias)
+#test_X = test_data
 
-pred = classifier.predict(Xtest)
-test_ids = np.load('test_id.npy')
-guess = le.inverse_transform(pred)
-final = create_submission(test_ids, guess)
+pred = classifier.predict(test_X)
+train_ids, train_cuisines, train_ingredients = read_data('train.json')
+test_ids, test_cuisines, test_ingredients = read_data('test.json')
+del train_ids, train_ingredients, test_cuisines, test_ingredients
+le = LabelEncoder()
+le.fit(train_cuisines)
+pred = le.inverse_transform(pred)
+create_submission(test_ids, pred)
 
 
 
